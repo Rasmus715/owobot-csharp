c6ff1477337aef984ab1eff5f9e2cc6a68fde251
Ivan Zatisev <i.zatsev@dreamsoft.by>
2022-05-23 10:07:27 +0300

    Fix docker-compose.yaml

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -5,15 +5,13 @@ namespace owobot_csharp.Data;
 
 public class ApplicationContext : DbContext
 {
-    public ApplicationContext()
-    {
-            
-    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite($"DataSource={Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}/Database.db");
+        optionsBuilder.UseSqlite("DataSource=Database.db");
+        //optionsBuilder.UseSqlite($"DataSource={Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}/Database.db");
         base.OnConfiguring(optionsBuilder);
     }
     
-    public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; }
 }
