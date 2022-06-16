435ad0d216418e183440e8cb0ac6e45d220dbc06
Ivan Zaitsev <35352131+Rasmus715@users.noreply.github.com>
2022-06-16 09:45:45 +0300

    Update Configuration.cs

--- Configuration.cs
+++ Configuration.cs
@@ -4,15 +4,15 @@ public class Configuration
 {
     public class Telegram
     {
-        public const string TelegramToken = "5378235767:AAGNBiePIq5UqZVUKx4-qdeaF7QOZeVI5FM";
+        public const string TelegramToken = "";
         public const string BotVersion = "v0.1";
     }
 
     public class Reddit
     {
-        public const string RedditAppId = "uU59EoywSUFJPc6t1DFX-w";
-        public const string RedditSecret = "XdTT8-a2GON4U9NBQGb0pCs7TAhTQA";
-        public const string RedditRefreshToken = "1793999091867-jyFmJj1SLY7JPRBNdFHiH8FOvB4NuQ";
+        public const string RedditAppId = "";
+        public const string RedditSecret = "";
+        public const string RedditRefreshToken = "";
     }
     
 }
